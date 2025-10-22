d5d5d6a2498a36f0aa4cfcc23bb949a9dbddb4ed
JarYin <tempest353@hotmail.com>
2025-10-22 15:33:21 +0700

    feat: update cookie settings to use 'none' for sameSite attribute; remove unused axios interceptors

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,4 @@
 import axios from "axios";
-import { getSession } from "./session";
-import Router from "next/router";
 
 const apiClient = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + "/api",
@@ -9,24 +7,5 @@ const apiClient = axios.create({
     },
     withCredentials: true,
 });
-
-apiClient.interceptors.response.use(async (config) => {
-    const accessToken = await getSession();
-    if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-})
-
-apiClient.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            console.error("Unauthorized! Maybe session expired.");
-            Router.push('/');
-        }
-        return Promise.reject(error);
-    }
-);
 
 export default apiClient;
